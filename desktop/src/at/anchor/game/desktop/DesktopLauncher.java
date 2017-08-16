b4181b524cc6dc331ab03eec6f32721b100c12f6
wfx <wolfgang.morawetz@gmail.com>
2017-08-16 19:19:50 +0200

    quick resolution hack

--- desktop/src/at/anchor/game/desktop/DesktopLauncher.java
+++ desktop/src/at/anchor/game/desktop/DesktopLauncher.java
@@ -7,6 +7,12 @@ import at.anchor.game.EsthetiqueGems;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+
+		// config.title = "Esthetique GEms";
+		config.useGL30 = true;
+		config.width = 460;
+		config.height = 820;
+
 		new LwjglApplication(new EsthetiqueGems(), config);
 	}
 }
