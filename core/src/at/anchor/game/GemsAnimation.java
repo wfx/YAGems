475ccc2829e11e9b8bd000b7de752c3370240c73
wfx <wolfgang.morawetz@gmail.com>
2017-10-22 14:47:19 +0200

    Using TextureAtlas

--- core/src/at/anchor/game/GemsAnimation.java
+++ core/src/at/anchor/game/GemsAnimation.java
@@ -1,14 +1,17 @@
 package at.anchor.game;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class GemsAnimation {
 
     private EsthetiqueGems _game;
     private TextureRegion[] _imgGems;
+    private TextureAtlas _atlasGems;
     private int[] _posX;
     private int _posY;
 
@@ -23,13 +26,13 @@ public class GemsAnimation {
         _imgGems = new TextureRegion[7];
         _posX = new int[7];
 
-        _imgGems[0] = new TextureRegion(assetManager.get("img/gemWhite.png", Texture.class));
-        _imgGems[1] = new TextureRegion(assetManager.get("img/gemRed.png", Texture.class));
-        _imgGems[2] = new TextureRegion(assetManager.get("img/gemPurple.png", Texture.class));
-        _imgGems[3] = new TextureRegion(assetManager.get("img/gemOrange.png", Texture.class));
-        _imgGems[4] = new TextureRegion(assetManager.get("img/gemGreen.png", Texture.class));
-        _imgGems[5] = new TextureRegion(assetManager.get("img/gemYellow.png", Texture.class));
-        _imgGems[6] = new TextureRegion(assetManager.get("img/gemBlue.png", Texture.class));
+        _imgGems[0] = new TextureRegion(_game.getAtlasGems().findRegion("gemWhite"));
+        _imgGems[1] = new TextureRegion(_game.getAtlasGems().findRegion("gemRed"));
+        _imgGems[2] = new TextureRegion(_game.getAtlasGems().findRegion("gemPurple"));
+        _imgGems[3] = new TextureRegion(_game.getAtlasGems().findRegion("gemOrange"));
+        _imgGems[4] = new TextureRegion(_game.getAtlasGems().findRegion("gemGreen"));
+        _imgGems[5] = new TextureRegion(_game.getAtlasGems().findRegion("gemYellow"));
+        _imgGems[6] = new TextureRegion(_game.getAtlasGems().findRegion("gemBlue"));
 
 
         for (int i = 0; i < 7; ++i) {
